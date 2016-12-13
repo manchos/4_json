0003d992e617fbad5b1f877882fde47859e033ea
manchos <vladm.ru@gmail.com>
2016-12-13 21:47:25 +0300

    first

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,21 @@
 import json
+import os
 
 
-def load_data(filepath):
-    pass
+def load_data(filepath: object) -> object:
+    """Return the dictionary of bars."""
+    if not os.path.exists(filepath):
+        return None
+    with open(filepath, encoding='utf8') as file_handler:
+        return json.load(file_handler)
 
 
 def pretty_print_json(data):
-    pass
+    print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Enter the real path to json file with list of bars:')
+    jsonparsed = load_data(filepath)
+    pretty_print_json(jsonparsed)
+
