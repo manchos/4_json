990e225cf5e71a5e7aff1388643206b247fc75a0
mancho <vladm.ru@gmail.com>
2016-12-14 23:09:07 +0300

    Update pprint_json.py

--- pprint_json.py
+++ pprint_json.py
@@ -3,7 +3,7 @@ import os
 
 
 def load_data(filepath: object) -> object:
-    """Return the dictionary of jsons data"""
+    #Return the dictionary of jsons data
     if not os.path.exists(filepath):
         return None
     with open(filepath, encoding='utf8') as file_handler:
@@ -11,12 +11,11 @@ def load_data(filepath: object) -> object:
 
 
 def pretty_print_json(data):
-    """pretty print json data"""
     print(json.dumps(data, indent=4, sort_keys=True, ensure_ascii=False))
 
 
 if __name__ == '__main__':
     filepath = input('Enter the real path to json file:')
-    jsonparsed = load_data(filepath)
-    pretty_print_json(jsonparsed)
+    json_data = load_data(filepath)
+    pretty_print_json(json_data)
 
